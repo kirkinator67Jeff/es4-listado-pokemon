e28aaf016ab7bde211745ce84c47e8e51d3f5450
Andru Colque <andru.colque@inacapmail.cl>
2026-07-06 22:18:58 -0400

    init: estructura inicial del proyecto (Vite y componentes base)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className="main-content">
+        <p>Próximamente: listado de pokémon</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Pokedex Interactiva </h1>
+      <p className="integrantes">
+        Integrantes: Andru Colque, Nicolás Pérez
+      </p>
+    </header>
+  );
+}
+
+export default Header;
